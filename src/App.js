9f707abdb9b82c0ad846d326e54df723596c25ff
Phillip Pargmann <pargmann92@gmail.com>
2020-05-06 10:04:20 +0200

    ready for pwa

--- src/App.js
+++ src/App.js
@@ -1,243 +1,250 @@
-import React from 'react';
+import React from "react";
 
-import Game from './pages/game/game.component';
-import Start from './pages/start/start.component';
+import CardOverview from "./components/card-overview/card-overview.component";
+import StartMenu from "./components/start-menu/start-menu.component";
 
-import Angular from './assets/angular.svg';
-import Vue from './assets/vue.svg';
-import Ember from './assets/ember.svg';
-import Aurelia from './assets/aurelia.svg';
-import Backbone from './assets/backbone.svg';
-import ReactBadge from './assets/react.svg';
+import { cards } from "./data";
 
-import './App.scss';
+import "./App.scss";
 
 class App extends React.Component {
-	state = {
-		gameIsRunning: false,
-		playerTwoTurn: false,
-		score: {
-			playerOne: 0,
-			playerTwo: 0
-		},
-		players: 1, // 1 || 2
-		difficulty: 1, // 0 = Easy, 1 = Normal, 2 = Hard
-		firstCard: null,
-		secondCard: null,
-		cards: [
-			{
-				id: 0,
-				name: 'Angular',
-				img: Angular,
-				flipped: false
-			},
-			{
-				id: 1,
-				name: 'Angular',
-				img: Angular,
-				flipped: false
-			},
-			{
-				id: 2,
-				name: 'Aurelia',
-				img: Aurelia,
-				flipped: false
-			},
-			{
-				id: 3,
-				name: 'Aurelia',
-				img: Aurelia,
-				flipped: false
-			},
-			{
-				id: 4,
-				name: 'Backbone',
-				img: Backbone,
-				flipped: false
-			},
-			{
-				id: 5,
-				name: 'Backbone',
-				img: Backbone,
-				flipped: false
-			},
-			{
-				id: 6,
-				name: 'Ember',
-				img: Ember,
-				flipped: false
-			},
-			{
-				id: 7,
-				name: 'Ember',
-				img: Ember,
-				flipped: false
-			},
-			{
-				id: 8,
-				name: 'React',
-				img: ReactBadge,
-				flipped: false
-			},
-			{
-				id: 9,
-				name: 'React',
-				img: ReactBadge,
-				flipped: false
-			},
-			{
-				id: 10,
-				name: 'Vue',
-				img: Vue,
-				flipped: false
-			},
-			{
-				id: 11,
-				name: 'Vue',
-				img: Vue,
-				flipped: false
-			}
-		]
-	};
+  state = {
+    gameIsRunning: false,
+    gameIsOver: false,
+    playerTwoTurn: false,
+    score: {
+      playerOne: 0,
+      playerTwo: 0,
+    },
+    players: 1, // 1 || 2
+    difficulty: 1, // 0 = Easy, 1 = Normal, 2 = Hard
+    firstCard: null,
+    secondCard: null,
 
-	componentDidMount() {
-		const { cards } = this.state;
+    cards: cards,
+  };
 
-		const shuffledCards = cards.map((card) => {
-			return {
-				...card,
-				order: Math.floor(Math.random() * cards.length)
-			};
-		});
+  componentDidMount() {
+    const { cards } = this.state;
 
-		this.setState({
-			...this.state,
-			cards: [ ...shuffledCards ]
-		});
-	}
+    const shuffledCards = cards.map((card) => {
+      return {
+        ...card,
+        order: Math.floor(Math.random() * cards.length),
+      };
+    });
 
-	startGameHandler = () => {
-		this.setState({
-			...this.state,
-			gameIsRunning: !this.state.gameIsRunning
-		});
-	};
+    this.setState({
+      ...this.state,
+      cards: [...shuffledCards],
+    });
+  }
 
-	playerHandler = (playerNumber) => {
-		if (this.state.players + playerNumber < 1 || this.state.players + playerNumber > 2) return;
-		this.setState({
-			...this.state,
-			players: this.state.players + playerNumber
-		});
-	};
+  startGameHandler = () => {
+    this.setState({
+      ...this.state,
+      gameIsRunning: !this.state.gameIsRunning,
+    });
+  };
 
-	difficultyHandler = (difficulty) => {
-		if (this.state.difficulty + difficulty < 0 || this.state.difficulty + difficulty > 2) return;
-		this.setState({
-			...this.state,
-			difficulty: this.state.difficulty + difficulty
-		});
-	};
+  resetGameStats = () => {
+    const { cards } = this.state;
 
-	resetFirstAndSecondCard = () => {
-		const state = { ...this.state };
+    const shuffledCards = cards.map((card) => {
+      return {
+        ...card,
+        order: Math.floor(Math.random() * cards.length),
+        flipped: false,
+      };
+    });
 
-		state.firstCard = null;
-		state.secondCard = null;
+    this.setState({
+      ...this.state,
+      gameIsRunning: false,
+      playerTwoTurn: false,
+      score: {
+        playerOne: 0,
+        playerTwo: 0,
+      },
+      players: 1, // 1 || 2
+      difficulty: 1, // 0 = Easy, 1 = Normal, 2 = Hard
+      firstCard: null,
+      secondCard: null,
+      gameIsOver: false,
+      cards: [...shuffledCards],
+    });
+  };
 
-		this.setState({
-			...state
-		});
-	};
+  playerHandler = (playerNumber) => {
+    if (
+      this.state.players + playerNumber < 1 ||
+      this.state.players + playerNumber > 2
+    )
+      return;
+    this.setState({
+      ...this.state,
+      players: this.state.players + playerNumber,
+    });
+  };
 
-	flipHandler = (id) => {
-		const state = { ...this.state };
+  difficultyHandler = (difficulty) => {
+    if (
+      this.state.difficulty + difficulty < 0 ||
+      this.state.difficulty + difficulty > 2
+    )
+      return;
+    this.setState({
+      ...this.state,
+      difficulty: this.state.difficulty + difficulty,
+    });
+  };
 
-		if (state.cards[id].flipped) {
-			return;
-		}
+  resetFirstAndSecondCard = () => {
+    const state = { ...this.state };
 
-		if (state.firstCard === null) {
-			state.cards[id].flipped = true;
-			state.firstCard = { ...state.cards[id] };
-			return this.setState({
-				...state
-			});
-		}
+    state.firstCard = null;
+    state.secondCard = null;
 
-		if (state.firstCard !== null && state.secondCard === null && id !== state.firstCard.id) {
-			state.cards[id].flipped = true;
-			state.secondCard = { ...state.cards[id] };
-		}
+    this.setState({
+      ...state,
+    });
+  };
 
-		// checkMatch
-		if (state.firstCard.name === state.secondCard.name) {
-			// clean first and secound, score + 1
-			state.firstCard = null;
-			state.secondCard = null;
-			if (this.state.playerTwoTurn) {
-				state.score.playerTwo += 1;
-			} else {
-				state.score.playerOne += 1;
-			}
-		} else {
-			// set flipped to false
-			setTimeout(() => {
-				state.cards[state.firstCard.id].flipped = false;
-				state.cards[state.secondCard.id].flipped = false;
-				state.playerTwoTurn = !state.playerTwoTurn;
-				state.firstCard = null;
-				state.secondCard = null;
+  checkWinner = (scorePlayerOne, scorePlayerTwo) => {
+    let total = scorePlayerOne + scorePlayerTwo;
+    let checkSum =
+      this.state.difficulty === 0
+        ? this.state.cards.length / 6 // 3
+        : this.state.difficulty === 1
+        ? this.state.cards.length / 3 // 6
+        : this.state.cards.length / 2; // 9
 
-				this.setState({
-					...state
-				});
-			}, 750);
-		}
+    if (total === checkSum) {
+      return true;
+    }
+    return false;
+  };
 
-		this.setState({
-			...state
-		});
-	};
+  flipHandler = (id) => {
+    const state = { ...this.state };
+    if (state.cards[id].flipped) return;
 
-	render() {
-		const { gameIsRunning, players, difficulty, cards, score } = this.state;
+    if (state.firstCard === null) {
+      state.cards[id].flipped = true;
+      return this.setState({
+        ...state,
+        firstCard: { ...state.cards[id] },
+      });
+    }
 
-		let cardValue = difficulty === 0 ? cards.length / 2 : difficulty === 1 ? cards.length / 3 : -cards.length;
+    if (
+      state.firstCard !== null &&
+      state.secondCard === null &&
+      id !== state.firstCard.id
+    ) {
+      state.cards[id].flipped = true;
+      state.secondCard = { ...state.cards[id] };
+    }
 
-		return (
-			<div className="App">
-				{gameIsRunning ? (
-					<React.Fragment>
-						<div className={players === 2 ? 'score' : 'score hide'}>
-							<h1 className="player-one">Player One: {score.playerOne}</h1>
-							<h4>
-								{!this.state.playerTwoTurn ? (
-									'Player One, it`s your turn!'
-								) : (
-									'Player Two, it`s your turn!'
-								)}
-							</h4>
-							<h1 className="player-two">Player Two: {score.playerTwo}</h1>
-						</div>
-						<Game
-							flipHandler={this.flipHandler}
-							cards={cards.slice(0, -cardValue)}
-							difficulty={difficulty}
-						/>
-					</React.Fragment>
-				) : (
-					<Start
-						startGame={this.startGameHandler}
-						setPlayers={this.playerHandler}
-						setDifficulty={this.difficultyHandler}
-						players={players}
-						difficulty={difficulty}
-					/>
-				)}
-			</div>
-		);
-	}
+    // checkMatch
+    if (state.firstCard.name === state.secondCard.name) {
+      // clean first and secound, score + 1
+      if (this.state.playerTwoTurn) {
+        state.score.playerTwo += 1;
+      } else {
+        state.score.playerOne += 1;
+      }
+    } else {
+      // set flipped to false
+      setTimeout(() => {
+        state.cards[state.firstCard.id].flipped = false;
+        state.cards[state.secondCard.id].flipped = false;
+        // if (state.players === 2) state.playerTwoTurn = !state.playerTwoTurn;
+
+        this.setState({
+          ...state,
+          playerTwoTurn:
+            state.players === 2 ? !state.playerTwoTurn : state.playerTwoTurn,
+          firstCard: null,
+          secondCard: null,
+        });
+      }, 750);
+    }
+
+    this.setState({
+      ...state,
+      firstCard: null,
+      secondCard: null,
+      gameIsOver: this.checkWinner(
+        state.score.playerOne,
+        state.score.playerTwo
+      ),
+    });
+  };
+
+  render() {
+    const {
+      gameIsRunning,
+      players,
+      difficulty,
+      cards,
+      score,
+      gameIsOver,
+    } = this.state;
+
+    let cardValue =
+      difficulty === 0
+        ? cards.length - 12
+        : difficulty === 1
+        ? cards.length - 6
+        : cards.length;
+
+    let evaluateWinner = () => {
+      if (players === 1) return <h1>You finished the game!</h1>;
+      if (score.playerOne === score.playerTwo) return <h1>It´s a draw!</h1>;
+      if (score.playerOne > score.playerTwo) return <h1>Player one won!</h1>;
+      if (score.playerTwo > score.playerOne) return <h1>Player two won!</h1>;
+    };
+
+    return (
+      <div className="App">
+        {gameIsRunning ? (
+          gameIsOver ? (
+            <React.Fragment>
+              {evaluateWinner()}
+              <p>Go back to</p>
+              <button onClick={this.resetGameStats}>Main Menu</button>
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              <div className={players === 2 ? "score" : "score hide"}>
+                <h1 className="player-one">Player One: {score.playerOne}</h1>
+                <h4>
+                  {!this.state.playerTwoTurn
+                    ? "Player One, it`s your turn!"
+                    : "Player Two, it`s your turn!"}
+                </h4>
+                <h1 className="player-two">Player Two: {score.playerTwo}</h1>
+              </div>
+              <CardOverview
+                flipHandler={this.flipHandler}
+                cards={cards.slice(0, cardValue)}
+                difficulty={difficulty}
+              />
+            </React.Fragment>
+          )
+        ) : (
+          <StartMenu
+            startGame={this.startGameHandler}
+            setPlayers={this.playerHandler}
+            setDifficulty={this.difficultyHandler}
+            players={players}
+            difficulty={difficulty}
+          />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,120 @@
+import Angular from "./assets/angular.svg";
+import Vue from "./assets/vue.svg";
+import Ember from "./assets/ember.svg";
+import Aurelia from "./assets/aurelia.svg";
+import Backbone from "./assets/backbone.svg";
+import ReactBadge from "./assets/react.svg";
+import Sass from "./assets/sass-1.svg";
+import Graphql from "./assets/graphql.svg";
+import NodeJS from "./assets/nodejs-1.svg";
+
+export const cards = [
+  {
+    id: 0,
+    name: "Angular",
+    img: Angular,
+    flipped: false,
+  },
+  {
+    id: 1,
+    name: "Angular",
+    img: Angular,
+    flipped: false,
+  },
+  {
+    id: 2,
+    name: "Aurelia",
+    img: Aurelia,
+    flipped: false,
+  },
+  {
+    id: 3,
+    name: "Aurelia",
+    img: Aurelia,
+    flipped: false,
+  },
+  {
+    id: 4,
+    name: "Backbone",
+    img: Backbone,
+    flipped: false,
+  },
+  {
+    id: 5,
+    name: "Backbone",
+    img: Backbone,
+    flipped: false,
+  },
+  {
+    id: 6,
+    name: "Ember",
+    img: Ember,
+    flipped: false,
+  },
+  {
+    id: 7,
+    name: "Ember",
+    img: Ember,
+    flipped: false,
+  },
+  {
+    id: 8,
+    name: "React",
+    img: ReactBadge,
+    flipped: false,
+  },
+  {
+    id: 9,
+    name: "React",
+    img: ReactBadge,
+    flipped: false,
+  },
+  {
+    id: 10,
+    name: "Vue",
+    img: Vue,
+    flipped: false,
+  },
+  {
+    id: 11,
+    name: "Vue",
+    img: Vue,
+    flipped: false,
+  },
+  {
+    id: 12,
+    name: "Sass",
+    img: Sass,
+    flipped: false,
+  },
+  {
+    id: 13,
+    name: "Sass",
+    img: Sass,
+    flipped: false,
+  },
+  {
+    id: 14,
+    name: "Graphql",
+    img: Graphql,
+    flipped: false,
+  },
+  {
+    id: 15,
+    name: "Graphql",
+    img: Graphql,
+    flipped: false,
+  },
+  {
+    id: 16,
+    name: "NodeJS",
+    img: NodeJS,
+    flipped: false,
+  },
+  {
+    id: 17,
+    name: "NodeJS",
+    img: NodeJS,
+    flipped: false,
+  },
+];
