7cbbc9b6d019ac6ec32ae0d5df6d081bd0be6654
Phillip Pargmann <pargmann92@gmail.com>
2020-05-06 10:36:06 +0200

    new icon

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import badge from '../../assets/js-badge.svg';
+import badge from "../../assets/js-badge.svg";
 
-import './card.styles.scss';
+import "./card.styles.scss";
 
 const Card = ({ card, flipHandler }) => {
-	return (
-		<div
-			className={card.flipped ? 'card flip' : 'card'}
-			onClick={() => flipHandler(card.id)}
-			style={{
-				order: card.order
-			}}
-		>
-			<img className="front-face" src={card.img} alt={card.name} />
-			<img className="back-face" src={badge} alt="JS-Badge" />
-		</div>
-	);
+  return (
+    <div
+      className={card.flipped ? "card flip" : "card"}
+      onClick={() => flipHandler(card.id)}
+      style={{
+        order: card.order,
+      }}
+    >
+      <img className="front-face" src={card.img} alt={card.name} />
+      <img className="back-face" src={badge} alt="JS-Badge" />
+    </div>
+  );
 };
 
 export default Card;
